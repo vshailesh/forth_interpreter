c01454ffe8826fc08f82a03faa30030a3e72cfa5
vshailesh <shavashishth@gmail.com>
2026-09-22 05:36:54 +0530

    Modified tests to match the latest changes.
    - tests modified because the parser returns an atom for each of the
      operation type instead of the operator wrapped in a string. For
      example a addition operator symbol will be represented by the atom
      :add instead of + sign.
    
    Signed-off-by: vshailesh <shavashishth@gmail.com>

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -4,26 +4,26 @@ defmodule ParserTest do
   test "simple expression parsing" do
     alias ForthInterpreter.EntryPoint
     {:ok, parsed, rest, _, _, _} = EntryPoint.expr("1 2 +")
-    assert parsed === [number: 1, number: 2, add: "+"]
+    assert parsed === [number: 1, number: 2, op: :add]
   end
 
   test "larger arithmetic expression parsing" do
     alias ForthInterpreter.EntryPoint
     {:ok, parsed, rest, _, _, _} = EntryPoint.expr("1 2 + 3 *")
-    assert parsed === [number: 1, number: 2, add: "+", number: 3, multiplication: "*"]
+    assert parsed === [number: 1, number: 2, op: :add, number: 3, op: :multiplication]
   end
 
   test "complex arithmetic expression parsing" do
     alias ForthInterpreter.EntryPoint
     {:ok, parsed, rest, _, _, _} = EntryPoint.expr("1 2 + 3 * 4 -")
     assert parsed === [
-      number: 1,
-      number: 2,
-      add: "+",
-      number: 3,
-      multiplication: "*",
-      number: 4,
-      subtraction: "-"
-    ]
+    number: 1,
+    number: 2,
+    op: :add,
+    number: 3,
+    op: :multiplication,
+    number: 4,
+    op: :subtraction
+  ]
   end
 end
